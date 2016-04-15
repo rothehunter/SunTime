2e4b37d4683fccdd1ebb93436392d01d120a48f9
Adrian Maniu <maniu17@yahoo.com>
2016-04-15 23:25:57 +0300

    Add To Git

---
+++ SunTimes/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DateTime sunrise = ;
+            //DateTime sunset = ;
+
+            SunTimes daylight = new SunTimes(
+                new DateTime(2015, 11, 27, 8, 15, 0),
+                new DateTime(2015, 11, 27, 18, 30, 10));
+
+            Console.WriteLine("Duration of day in minutes is: {0}", daylight.DaylightMinutes());
+
+            ShoppingCart myCart = new ShoppingCart();
+
+            myCart.AddProduct(20);
+            myCart.AddProduct(30);
+
+            myCart.Checkout();
+
+            Console.ReadLine();
+        }
+    }
+
+    class SunTimes
+    {
+        private DateTime SunriseTime { get; set; }
+        private DateTime SunsetTime { get; set; }
+
+        public SunTimes(DateTime sunrise, DateTime sunset)
+        {
+            SunriseTime = sunrise;
+            SunsetTime = sunset;
+        }
+
+        public double DaylightMinutes()
+        {
+            return SunsetTime.Subtract(SunriseTime).TotalMinutes;
+        } 
+    }
+
+    class ShoppingCart
+    {
+        public int TotalPrice { get; set; }
+        public int ProductCount { get; set; }
+
+        public void AddProduct(int price)
+        {
+            TotalPrice += price;
+            ProductCount++;
+        }
+
+        public void Checkout()
+        {
+            Console.WriteLine("Total price is: " + TotalPrice);
+            Console.WriteLine("Product count is: " + ProductCount);
+            Console.WriteLine("Average price is: " + TotalPrice / ProductCount);
+        }
+    }
+}
---
+++ SunTimes/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SunTimes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SunTimes")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a763faa7-6dbf-4421-90e5-cd005ea1b1be")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
